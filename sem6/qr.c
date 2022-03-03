097f52fb460b9091a8d330166760e56e71d942b9
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 22:16:55 +0300

    Update qr.c

--- sem6/qr.c
+++ sem6/qr.c
@@ -129,9 +129,8 @@ void restore_q(double* q, double* restored_q, int n) {
                     int col_abs = jb - j;
                     if (row_abs > col_abs) {
                         double c = q[row_abs*n + col_abs];
-                        double s = q[col_abs*n + row_abs];
-                        for (int k = 0; k < n; ++k)
-                            rotate(&restored_q[col_abs*n + k], &restored_q[row_abs*n + k], c, s);
+                        double s = -q[col_abs*n + row_abs];
+                        cblas_drot(n, &restored_q[col_abs*n], 1, &restored_q[row_abs*n], 1, c, s);
                     }
                 }
             }
