18a602192c3079089ba960cf7a538637200e2e92
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 20:43:27 +0300

    Update qr.c

--- v2.0/qr.c
+++ v2.0/qr.c
@@ -1,18 +1,7 @@
 #include <cblas.h>
 #include <math.h>
+#include "qr_batch.h"
 #include "qr.h"
-
-void compute_params(double aii, double aji, double* c, double* s) {
-    *c = aii / sqrt(aii*aii + aji*aji);
-    *s = -aji / sqrt(aii*aii + aji*aji);
-}
-
-void rotate(double* xi, double* xj, double c, double s) {
-    double xi_ = (*xi)*c - (*xj)*s;
-    double xj_ = (*xi)*s + (*xj)*c;
-    *xi = xi_;
-    *xj = xj_;
-}
 
 void qr(double* a, double* q, int n) {
     for (int j = 0; j < n - 1; ++j) {
