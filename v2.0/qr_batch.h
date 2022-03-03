a404461552dd725d912d518b8a173247379aa4e1
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 16:54:27 +0300

    Update qr_batch.h

--- v2.0/qr_batch.h
+++ v2.0/qr_batch.h
@@ -1,7 +1,7 @@
 #ifndef _QRBATCH_
 #define _QRBATCH_
 
-void qr_batch(ELEMENT_TYPE* a, ELEMENT_TYPE* q, int n);
+void qr_batch(double* a, double* q, int n);
 void restore_q_batch(double* q, double* restored_q, int n)
 
 #endif
