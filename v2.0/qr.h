9e618dcaf4e4b0f5b46f9604f8d0de99a90f26a2
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 17:08:29 +0300

    Update qr.h

--- v2.0/qr.h
+++ v2.0/qr.h
@@ -1,6 +1,8 @@
 #ifndef _QR_
 #define _QR_
 
+void compute_params(double aii, double aji, double* c, double* s);
+void rotate(double* xi, double* xj, double c, double s);
 void qr(double* a, double* q, int n);
 void restore_q(double* q, double* restored_q, int n);
 
