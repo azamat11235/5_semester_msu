65ffcd645997077ce75347de9b82de19274868e7
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 21:16:35 +0300

    Update debug.c

--- v2.0/debug.c
+++ v2.0/debug.c
@@ -12,6 +12,7 @@
 typedef void qr_func_type(double*, double*, int);
 typedef void restore_q_func_type(double*, double*, int);
 
+
 double compute_time(qr_func_type* qr_func, int size) {
     const int num_of_iters = 5;
     double t = 0;
