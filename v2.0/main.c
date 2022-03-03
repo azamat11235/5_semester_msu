33153129ede3ebbae46eda362ba9662f1fa08fca
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 20:46:14 +0300

    Update main.c

--- v2.0/main.c
+++ v2.0/main.c
@@ -1,49 +1,14 @@
 #include <stdio.h>
 #include "qr.h"
-/*
 #include "qr_batch.h"
 #include "qr_cblas.h"
 #include "qr_lapack.h"
 #include "debug.h"
-*/
 
-#include "qr_batch.h"
-
-#include <math.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
-#include "routine.h"
-#include "parameters.h"
-
-
-typedef void qr_func_type(double*, double*, int);
-typedef void restore_q_func_type(double*, double*, int);
-
-double compute_time(qr_func_type* qr_func, int size) {
-    const int num_of_iters = 5;
-    double t = 0;
-    double* matrix;
-    double* q;
-    allocMatrix(&q, size);
-    allocMatrix(&matrix, size);
-    for (int i = 0; i < num_of_iters; ++i) {
-        clock_t start;
-        clock_t end;
-        fillMatrix(matrix, size);
-        start = clock();
-        (*qr_func)(matrix, q, size);
-        end = clock();
-        t += end - start;
-    }
-    free(matrix);
-    free(q);
-    return t / num_of_iters / CLOCKS_PER_SEC;
-}
 
 int main(int argv, char** argc) {
     for (int n = 256; n <= 2048; n *= 2)
-        printf("size = %4d, time (qr_batch):   %f s.\n", n,  compute_time(&qr, n));
+        printf("size = %4d, time (qr_batch):   %f s.\n", n,  compute_time(&qr_batch, n));
     /*
     printf("-------------------------------------------\n");
     printf("tests:\n");
