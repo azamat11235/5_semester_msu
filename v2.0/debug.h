fdadda971ffb8168f7203fc17c3b3fd3cadb6a01
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 16:56:50 +0300

    Update debug.h

--- v2.0/debug.h
+++ v2.0/debug.h
@@ -1,13 +1,6 @@
 #ifndef _DEBUG_
 #define _DEBUG_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <math.h>
-#include "routine.h"
-
 typedef void qr_func_type(double*, double*, int);
 typedef void restore_q_func_type(double*, double*, int);
 double compute_time(qr_func_type* qr_func, int size);
