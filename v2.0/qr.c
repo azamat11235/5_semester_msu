c8eca98d1eb8a5cb58b6be00f43cdab204eacc39
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 16:53:44 +0300

    Update qr.c

--- v2.0/qr.c
+++ v2.0/qr.c
@@ -1,3 +1,4 @@
+#include <cblas.h>
 #include math.h
 #include "qr.h"
 
@@ -28,16 +29,14 @@ void qr(double* a, double* q, int n) {
 }
 
 void restore_q(double* q, double* restored_q, int n) {
-    double* restored_q;
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
             restored_q[n * i + j] = (double)(i == j);
     for (int j = n-2; j >= 0; --j) {
         for (int i = n-1; i > j; --i) {
             double c = q[i*n + j];
-            double s = q[j*n + i]
+            double s = q[j*n + i];
             cblas_drot(n-i, &restored_q[j*n + j], 1, &restored_q[i*n + j], 1, c, s);
         }
     }
-    return res;
 }
