7fe6b14925e2172f7b80e1bda4f163a8676aaffe
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-03 21:06:30 +0300

    Update qr.c

--- v2.0/qr.c
+++ v2.0/qr.c
@@ -1,18 +1,29 @@
 #include <cblas.h>
 #include <math.h>
-#include "qr_batch.h"
 #include "qr.h"
 
+
+void rotate(double* xi, double* xj, double c, double s) {
+    double xi_ = (*xi)*c - (*xj)*s;
+    double xj_ = (*xi)*s + (*xj)*c;
+    *xi = xi_;
+    *xj = xj_;
+    
 void qr(double* a, double* q, int n) {
     for (int j = 0; j < n - 1; ++j) {
         for (int i = j + 1; i < n; ++i) {
-            double c;
-            double s;
-            compute_params(a[j*n + j], a[i*n + j], &c, &s);
+            double ajj =  a[j*n + j];
+            double aij =  a[i*n + j];
+            double c = aii / sqrt(aii*aii + aji*aji);
+            double s = -aji / sqrt(aii*aii + aji*aji);
             q[i*n + j] = c;
             q[j*n + i] = s;
-            for (int k = j; k < n; ++k)
-                rotate(&a[j*n + k], &a[i*n + k], c, s);
+            for (int k = j; k < n; ++k) {
+                double ajk = a[j*n + k];
+                double aik = a[i*n + k];
+                a[j*n + k] = aik*c - ajk*s;
+                a[i*n + k] = aik*s + ajk*c;
+            }
         }
     }
 }
@@ -25,8 +36,12 @@ void restore_q(double* q, double* restored_q, int n) {
         for (int i = n-1; i > j; --i) {
             double c = q[i*n + j];
             double s = -q[j*n + i];
-            for (int k = j; k < n; ++k)
-                rotate(&restored_q[j*n + k], &restored_q[i*n + k], c, s);
+            for (int k = j; k < n; ++k) {
+                double qjk = restored_q[j*n + k];
+                double qik = restored_q[i*n + k];
+                restored_q[j*n + k] = qik*c - qjk*s;
+                restored_q[i*n + k] = qik*s + qjk*c;
+            }
         }
     }
 }
