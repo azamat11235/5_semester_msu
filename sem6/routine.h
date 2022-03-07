f12ac3d504e08ebb600c8d33edac39c1d96c90be
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-07 12:28:25 +0300

    Update routine.h

--- sem6/routine.h
+++ sem6/routine.h
@@ -1,6 +1,8 @@
 #ifndef _ROUTINE_
 #define _ROUTINE_
 
+void bcache(double* a, int na, double* cache, int i, int j, int k); //
+void bflush(double* a, int na, double* cache, int i, int j, int k); //
 void allocMatrix(double** matrix, int n);
 void fillMatrix(double* matrix, int n);
 int are_equal(double* a, double* b, int n, double eps);
