d8428a24e4582378f25c73bb53a49e706459aa6b
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-07 12:21:53 +0300

    Update qr.h

--- sem6/qr.h
+++ sem6/qr.h
@@ -3,9 +3,9 @@
 
 void qr(double* a, double* q, int n);
 void restore_q(double* q, double* restored_q, int n);
-void compute_params(double aii, double aji, double* c, double* s);  //
-void rotate(double* xi, double* xj, double c, double s);            //
-void bcache(double* a, int na, double* cache, int i, int j, int k); //
-void bflush(double* a, int na, double* cache, int i, int j, int k); //
+// void compute_params(double aii, double aji, double* c, double* s);  //
+// void rotate(double* xi, double* xj, double c, double s);            //
+// void bcache(double* a, int na, double* cache, int i, int j, int k); //
+// void bflush(double* a, int na, double* cache, int i, int j, int k); //
 
 #endif
