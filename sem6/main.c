3f7a3ddf80e3432bb29659cc2522138abfc7b52b
azamat11235 <82286467+azamat11235@users.noreply.github.com>
2022-03-07 12:19:34 +0300

    Update main.c

--- sem6/main.c
+++ sem6/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "debug.h"
 #include "qr.h"
+#include "qr_omp.h"
 
 int main() {
     if (test(qr) == 0)
         printf("test: OK\n");
     for (int n = 256; n <= 2048; n *= 2)
-        printf("size = %4d, time = %f\n", n, compute_time(qr, n));
+        printf("size = %4d, time = %f\n", n, compute_time(qr)omp, n));
     return 0;
 }
