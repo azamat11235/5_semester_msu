82867a129d5966e9be1d55976cb039724556b0ac
Azamat Sultonov <azamatsultonov42@gmail.com>
2022-03-07 12:11:23 +0300

    .

--- sem6/qr.c
+++ sem6/qr.c
@@ -1,18 +1,8 @@
 #include <math.h>
+#include <cblas.h>
 #include "qr.h"
 #include "parameters.h"
 
-void compute_params(double aii, double aji, double* c, double* s) {
-    *c = aii / sqrt(aii * aii + aji * aji);
-    *s = -aji / sqrt(aii * aii + aji * aji);
-}
-
-void rotate(double* xi, double* xj, double c, double s) {
-    double xi_ = (*xi) * c - (*xj) * s;
-    double xj_ = (*xi) * s + (*xj) * c;
-    *xi = xi_;
-    *xj = xj_;
-}
 
 void bcache(double* a, int na, double* cache, int i, int j, int k) {
     for (int ii = 0; ii < _b; ++ii) {
@@ -41,41 +31,37 @@ void qr(double* a, double* q, int n) {
         // вращаем диаг. блок
         for (int j = 0; j < _b-1; ++j) {
             for (int i = j+1; i < _b; ++i) {
-                double ajj = cache[2*_b*_b + j*_b + j];
-                double aij = cache[2*_b*_b + i*_b + j];
+		int jj = 2*_b*_b + j*_b + j;
+		int ij = 2*_b*_b + i*_b + j;
+                double ajj = cache[jj];
+                double aij = cache[ij];
                 double c;
                 double s;
-                compute_params(ajj, aij, &c, &s);
-                cache[i*_b + j] = c;
-                cache[j*_b + i] = s;
-                for (int k = j; k < _b; ++k) {
-                    int jk = 2*_b*_b + j*_b + k;
-                    int ik = 2*_b*_b + i*_b + k;
-                    rotate(&cache[jk], &cache[ik], c, s);
-                }
-
+                cblas_drotg(&ajj, &aij, &c, &s);
+		cache[i*_b + j] = c;
+		cache[j*_b + i] = -s;
+		cblas_drot(_b-j, &cache[jj], 1, &cache[ij], 1, c, s);
             }
         }
         bflush(q, n, cache, jb, jb, 0); // sin, cos
         // вращаем столбец (поддиаг. блоки)
         for (int ib = jb+_b; ib < n; ib += _b) {
             bcache(a, n, cache, ib, jb, 3); // поддиаг. блок
-            for (int j = 0; j < _b; ++j) {
+	    for (int j = 0; j < _b; ++j) {
+		int jj = 2*_b*_b + j*_b + j;
                 for (int i = 0; i < _b; ++i) {
-                    double ajj = cache[2*_b*_b + j*_b + j];
-                    double aij = cache[3*_b*_b + i*_b + j];
+		    int ij = 3*_b*_b + i*_b + j;
+                    double ajj = cache[jj];
+                    double aij = cache[ij];
                     double c;
                     double s;
-                    compute_params(ajj, aij, &c, &s);
-                    cache[i*_b + j] = c;
-                    cache[_b*_b + j*_b + i] = s;
-                    for (int k = j; k < _b; ++k) {
-                       int jk = 2*_b*_b + j*_b + k;
-                       int ik = 3*_b*_b + i*_b + k;
-                       rotate(&cache[jk], &cache[ik], c, s);
-                    }
-                }
-            }
+		    cblas_drotg(&ajj, &aij, &c, &s);
+		    cache[i*_b + j] = c;
+		    cache[_b*_b + j*_b + i] = -s;
+		    cblas_drot(_b-j, &cache[jj], 1, &cache[ij], 1, c, s);
+
+		}
+	    }
             bflush(q, n, cache, ib, jb, 0); // cos
             bflush(q, n, cache, jb, ib, 1); // sin
             bflush(a, n, cache, ib, jb, 3); // поддиаг. блок
@@ -90,14 +76,12 @@ void qr(double* a, double* q, int n) {
                 for (int i = j+1; i < _b; ++i) {
                     double c;
                     double s;
-                    c = cache[i*_b + j];
-                    s = cache[j*_b + i];
-                    for (int k = 0; k < _b; ++k) {
-                        int jk = 2*_b*_b + j*_b + k;
-                        int ik = 2*_b*_b + i*_b + k;
-                        rotate(&cache[jk], &cache[ik], c, s);
-                    }
-                }
+                    c =  cache[i*_b + j];
+                    s = -cache[j*_b + i];
+		    int jrow = 2*_b*_b + j*_b;
+		    int irow = 2*_b*_b + i*_b;
+		    cblas_drot(_b, &cache[jrow], 1, &cache[irow], 1, c, s);
+		}
             }
             // вращения поддиаг. блоков
             for (int ib = jb+_b; ib < n; ib += _b) {
@@ -109,12 +93,10 @@ void qr(double* a, double* q, int n) {
                         double c;
                         double s;
                         c = cache[i*_b + j];
-                        s = cache[_b*_b + j*_b + i];
-                        for (int k = 0; k < _b; ++k) {
-                            int jk = 2*_b*_b + j*_b + k;
-                            int ik = 3*_b*_b + i*_b + k;
-                            rotate(&cache[jk], &cache[ik], c, s);
-                        }
+                        s = -cache[_b*_b + j*_b + i];
+			int jrow = 2*_b*_b + j*_b;
+			int irow = 3*_b*_b + i*_b;
+			cblas_drot(_b, &cache[jrow], 1, &cache[irow], 1, c, s);
                     }
                 }
                 bflush(a, n, cache, ib, jb2, 3); // внедиаг. блок (нижний)
@@ -136,10 +118,9 @@ void restore_q(double* q, double* restored_q, int n) {
                     int col_abs = jb - j;
                     if (row_abs > col_abs) {
                         double c = q[row_abs*n + col_abs];
-                        double s = -q[col_abs*n + row_abs];
-                        for (int k = 0; k < n; ++k)
-                            rotate(&restored_q[col_abs*n + k], &restored_q[row_abs*n + k], c, s);
-                    }
+                        double s = q[col_abs*n + row_abs];
+                    	cblas_drot(n, &restored_q[col_abs*n], 1, &restored_q[row_abs*n], 1, c, s);
+		    }
                 }
             }
          }
